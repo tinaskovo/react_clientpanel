9849032562b920f38a0ef61fa3bfd19d0a5a83cc
Tina Skov Olesen <tinaskovo@gmail.com>
2018-10-31 11:53:52 +0100

    clients

--- src/components/clients/Clients.js
+++ src/components/clients/Clients.js
@@ -1,12 +1,66 @@
 import React, { Component } from "react";
+import { Link } from "react-router-dom";
 
 class Clients extends Component {
   render() {
-    return (
-      <div>
-        <h1>Clients</h1>
-      </div>
-    );
+    const clients = [
+      {
+        id: "1",
+        firstName: "Kevin",
+        lastName: "Johnson",
+        email: "kevin@gmail.com",
+        phone: "555555555",
+        balance: "30"
+      }
+    ];
+
+    if (clients) {
+      return (
+        <div>
+          <div className="row">
+            <div className="col-md-6">
+              <h2>
+                {" "}
+                <i className="fas fa-users">Clients</i>
+              </h2>
+            </div>
+            <div className="col-md-6" />
+          </div>
+          <table className="table table-striped">
+            <thead className="thead-inverse">
+              <tr>
+                <th>Name</th>
+                <th>Email</th>
+                <th>Balance</th>
+                <th />
+              </tr>
+            </thead>
+            <tbody>
+              {clients.map(client => (
+                <tr key={client.id}>
+                  <td>
+                    {client.firstName} {client.lastName}
+                  </td>
+                  <td>{client.email}</td>
+                  <td>${parseFloat(client.balance).toFixed(2)}</td>
+                  <td>
+                    <Link
+                      to={`/client/${client.id}`}
+                      className="btn btn-secondary btn-sm"
+                    >
+                      <i className="fas fa-arrow-circle-right" />
+                      Details
+                    </Link>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
+      );
+    } else {
+      return <h1>Loading...</h1>;
+    }
   }
 }
 

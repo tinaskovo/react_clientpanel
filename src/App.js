c625c73a323975cd72f84861d3bf0aa32c0c7c06
Tina Skov Olesen <tinaskovo@gmail.com>
2018-11-01 13:10:32 +0100

    full crud done

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import store from "./store";
 import AppNavbar from "./components/layout/AppNavbar";
 import Dashboard from "./components/layout/Dashboard";
 import AddClient from "./components/clients/AddClient";
+import EditClient from "./components/clients/EditClient";
+import ClientDetails from "./components/clients/ClientDetails";
 
 import "./App.css";
 
@@ -21,6 +23,8 @@ class App extends Component {
               <Switch>
                 <Route exact path="/" component={Dashboard} />
                 <Route exact path="/client/add" component={AddClient} />
+                <Route exact path="/client/edit/:id" component={EditClient} />
+                <Route exact path="/client/:id" component={ClientDetails} />
               </Switch>
             </div>
           </div>
